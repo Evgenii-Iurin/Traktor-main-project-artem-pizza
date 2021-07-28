51ce591063fd94e3988bc3ac0620c18349c15b44
Evgenii-Iurin <j89065689811.yurin@yandex.ru>
2021-07-28 15:35:41 +0300

    Adding a pizza configurator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import React from "react";
+import Configurator from "./Configurator";
+import YourOrder from "./FinalYourOrder";
 
-function App() {
+export default function App() {
+  const [cost, setCost] = React.useState(200);
+  const [selected, setSelected] = React.useState([]);
+  const [currentPrice, setCurrentPrice] = React.useState("");
+  const [title, setTitle] = React.useState("");
+  const formInput = React.useRef();
+
+  function finalCostOfOrder(event) {
+    event.preventDefault();
+    const item = formInput.current.querySelectorAll("input");
+    for (let i = 0; i < item.length; i++) {
+      if (item[i].checked) {
+        setSelected((selected) => [...selected, { value: item[i].value }]);
+      }
+    }
+
+    setCurrentPrice((currentPrice) => "Final price: " + cost);
+    setTitle((title) => <h3>Your order</h3>);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Configurator
+        cost={cost}
+        setCost={setCost}
+        onClick={finalCostOfOrder}
+        formInput={formInput}
+      />
+      <br />
+      <YourOrder
+        selected={selected}
+        currentPrice={currentPrice}
+        title={title}
+      />
     </div>
   );
 }
-
-export default App;
